f3c0cda3ef80bfdcad74ebd90edbfe4731825878
Kevin Nguyen <ktn1234@yahoo.com>
2024-11-17 17:03:50 -0600

    ♻️📱 update footer to use sitename PerplexityThat.com, update min screen size for footer
    874px is the min screen size width for all chars to be in the same row, the height of the footer with one row is 64px so subtract that from 100vh (100vh - 64px) vice/verse

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="flex flex-col text-center p-5 min-h-[calc(100vh_-88px)] min-[909px]:min-h-[calc(100vh_-64px)]">
+      <main className="flex flex-col text-center p-5 min-h-[calc(100vh_-88px)] min-[874px]:min-h-[calc(100vh_-64px)]">
         {children}
       </main>
       <Footer />
