2276c73646d4457dd89d4344752707f05a477d53
Kevin Nguyen <ktn1234@yahoo.com>
2024-11-17 01:08:29 -0600

    💄🔧 configure tailwindcss/css colors to perplexity ai's theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#191A1A", // Perplexity Black
+        secondary: "#202222", // Perplexity Dark Grey
+        tertiary: "#8D9191", // Perplexity Light Grey
+        quaterternary: "#249BAA", // Perplexity Blue
+      },
+    },
   },
   plugins: [],
 };
