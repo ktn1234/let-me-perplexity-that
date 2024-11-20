08fb97a4beb6fb9560be6abad263c4c4af19c196
Kevin Nguyen <ktn1234@yahoo.com>
2024-11-20 16:51:03 -0600

    📈 add vercel analytics component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { registerSW } from "virtual:pwa-register";
 
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>
 );
